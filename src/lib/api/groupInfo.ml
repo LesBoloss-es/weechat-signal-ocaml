113464e97d8fccc5954ecb075bc57f5e2e3371b9
Martin Pépin <kerl@wkerl.me>
2020-12-20 16:37:17 +0100

    The type field is optional in GroupInfo

--- src/lib/api/groupInfo.ml
+++ src/lib/api/groupInfo.ml
@@ -2,6 +2,12 @@ type t = {
   groupId:  string;
   members:  Address.t list option [@default None];
   name:     string option         [@default None];
-  typ:      string                [@key "type"];
+  typ:      string option         [@key "type"] [@default None];
   avatarId: Int64.t option        [@default None];
 } [@@deriving of_yojson, show]
+
+
+let of_yojson json =
+  match of_yojson json with
+  | Ok x -> Ok x
+  | Error _ -> Helpers.error "Could not parse %a as GroupInfo.t" Yojson.Safe.pp json
