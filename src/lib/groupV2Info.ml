d16ea87a6ecc2f7a45225c9087738690ebf76a45
Martin Pépin <kerl@wkerl.me>
2020-12-20 12:42:39 +0100

    Signald Json API: GroupV2Info

--- src/lib/groupV2Info.ml
+++ src/lib/groupV2Info.ml
@@ -1,5 +1,11 @@
-type t = unit
-(* TODO *)
-
-let of_yojson _ =
-  Error "Not implemented: GroupV2Info"
+type t = {
+  id:                 string option         [@default None];
+  inviteLinkPassword: string option         [@default None];
+  masterKey:          string option         [@default None];
+  members:            Address.t list option [@default None];
+  pendingMembers:     Address.t list option [@default None];
+  requestingMembers:  Address.t list option [@default None];
+  revision:           int option            [@default None];
+  timer:              int option            [@default None];
+  title:              string option         [@default None];
+} [@@deriving of_yojson]
