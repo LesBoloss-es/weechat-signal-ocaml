3eadaeb04658f0a2843e20193693d2da5d8ef0a7
Martin Pépin <kerl@wkerl.me>
2020-12-21 00:53:02 +0100

    Signald Json API: SentTranscriptMessage

--- src/lib/api/sentTranscriptMessage.ml
+++ src/lib/api/sentTranscriptMessage.ml
@@ -1,5 +1,21 @@
-type t = unit
-(* TODO *)
+type status_map = (string * bool) list
 
-let of_yojson _ =
-  Error "Not implemented: SentTranscriptMessage"
+let status_map_of_yojson json =
+  let open Helpers.Syntax in
+  match json with
+  | `Assoc assoc ->
+    Helpers.try_list_map
+      (fun (id, b) ->
+        let+ b = [%of_yojson: bool] b in
+        (id, b))
+      assoc
+  | _ -> Error "status_map"
+
+type t = {
+  destination:              Address.t option [@default None];
+  expirationStartTimestamp: Int64.t;
+  isRecipientUpdate:        bool;
+  message:                  DataMessage.t;
+  timestamp:                Int64.t;
+  unidentifiedStatus:       status_map       [@default []];
+} [@@deriving of_yojson]
