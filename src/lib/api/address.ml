238f1d7ad39b42fc3bde37bd601dd90c0a6cd356
Martin Pépin <kerl@wkerl.me>
2020-12-20 16:22:26 +0100

    Signald Json API: ContactInfo

--- src/lib/api/address.ml
+++ src/lib/api/address.ml
@@ -3,3 +3,10 @@ type t = {
   uuid: string option;
   relay: string option;
 } [@@deriving of_yojson, show]
+
+
+(** {2 Getters} *)
+
+let number addr = match addr.number with
+  | Some n -> Ok n
+  | None -> Helpers.error "No number of address %a" pp addr
---
+++ src/lib/api/contactInfo.ml
@@ -0,0 +1,14 @@
+type t = {
+  name:                  string option [@default None];
+  address:               Address.t;
+  color:                 string option [@default None];
+  profileKey:            string option [@default None];
+  messageExpirationTime: int option    [@default None];
+} [@@deriving of_yojson, show]
+
+
+(** {2 Getters} *)
+
+let name c = match c.name with
+  | Some n -> Ok n
+  | None -> Helpers.error "No name for contact %a" pp c
