6eb827418fbc2a592c843797d3ec1f135a803593
Martin Pépin <kerl@wkerl.me>
2020-12-20 12:31:18 +0100

    Signald Json API: DataMessage

--- src/lib/dataMessage.ml
+++ src/lib/dataMessage.ml
@@ -1,4 +1,17 @@
-type t = unit
-
-let of_yojson _ =
-  Error "Not implemented: dataMessage"
+type t = {
+  timestamp:        Int64.t;
+  attachments:      Attachment.t list     [@default []];
+  body:             string                [@default ""];
+  group:            GroupInfo.t option    [@default None];
+  groupV2:          GroupV2Info.t option  [@default None];
+  endSession:       bool;
+  expiresInSeconds: int option            [@default None];
+  profileKeyUpdate: bool;
+  quote:            Quote.t option        [@default None];
+  contacts:         SharedContact.t list  [@default []];
+  previews:         Preview.t list        [@default []];
+  sticker:          Sticker.t option      [@default None];
+  viewOnce:         bool;
+  reaction:         Reaction.t option     [@default None];
+  remoteDelete:     RemoteDelete.t option [@default None];
+} [@@ deriving of_yojson]
