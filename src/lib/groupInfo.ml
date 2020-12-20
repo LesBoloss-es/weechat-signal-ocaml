889841f2b795bba1e3ce4453335968033e5a31bf
Martin Pépin <kerl@wkerl.me>
2020-12-20 12:36:34 +0100

    Signald Json API: GroupInfo

--- src/lib/groupInfo.ml
+++ src/lib/groupInfo.ml
@@ -1,5 +1,7 @@
-type t = unit
-(* TODO *)
-
-let of_yojson _ =
-  Error "Not implemented: GroupInfo"
+type t = {
+  groupId:  string;
+  members:  Address.t list option [@default None];
+  name:     string option         [@default None];
+  typ:      string                [@key "type"];
+  avatarId: Int64.t option        [@default None];
+} [@@deriving of_yojson]
