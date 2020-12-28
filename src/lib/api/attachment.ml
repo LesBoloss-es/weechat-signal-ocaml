d204ef9fe8e924a56d2b45b51d3b3cb210abff89
Martin Pépin <kerl@wkerl.me>
2020-12-28 20:26:15 +0100

    Signald Json API: Attachment

--- src/lib/api/attachment.ml
+++ src/lib/api/attachment.ml
@@ -1,8 +1,15 @@
-type t = int
-(* TODO *)
-
-let of_yojson _ =
-  Error "Not implemeneted: Attachement"
-
-let to_yojson _ =
-  invalid_arg "Not implemented: Attachment"
+type t = {
+  contentType:    string;
+  id:             string;
+  key:            string;
+  size:           int option    [@default None];
+  digest:         string option [@default None];
+  voiceNote:      bool;
+  width:          int;
+  height:         int;
+  caption:        string option [@default None];
+  blurhash:       string option [@default None];
+  customFilename: string option [@default None];
+  storedFilename: string option [@default None];
+  filename:       string option [@default None];
+} [@@deriving yojson]
